85112936299249d91873074d8a04f22125d08c3c
VikingFishBird <oybas@hotmail.com>
2020-06-13 23:05:35 -0700

    SLIDERS

--- Insignificance/Assets/Scripts/CubeSpawner.cs
+++ Insignificance/Assets/Scripts/CubeSpawner.cs
@@ -6,6 +6,9 @@ public class CubeSpawner : MonoBehaviour
 {
     public Transform cube;
 
+    public Material grass;
+    public Material water;
+
     public void PlaceCubes(Coord[,] coords, Transform parent, float[,] perlin) {
         string holderName = "Object Holder";
         if (parent.Find(holderName)) {
@@ -25,6 +28,16 @@ public class CubeSpawner : MonoBehaviour
 
     public void PlaceCubeAtCoord(Coord coord, Transform parent, int heightLevel) {
         Transform cubey = Instantiate(cube, new Vector3(coord.x, -0.5f + heightLevel, coord.y), Quaternion.Euler(Vector3.right * -90));
+        if (heightLevel == 0) {
+            Material[] newMat = {water, water, water, water };
+            cubey.gameObject.GetComponent<Renderer>().materials = newMat;
+            cubey.localPosition = cubey.localPosition + Vector3.up;
+        }
+        else {
+            Material[] newMat = { grass, grass, grass, grass };
+            cubey.gameObject.GetComponent<Renderer>().materials = newMat;
+        }
+
         cubey.localScale = new Vector3(50f, 50f, 50f);
         cubey.parent = parent;
     }
