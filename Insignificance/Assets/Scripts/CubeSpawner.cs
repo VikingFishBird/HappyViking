63abb2f8c52b4985d94bfda345801ec8c300b708
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2020-06-13 18:24:01 -0700

    another one

--- Insignificance/Assets/Scripts/CubeSpawner.cs
+++ Insignificance/Assets/Scripts/CubeSpawner.cs
@@ -6,18 +6,6 @@ public class CubeSpawner : MonoBehaviour
 {
     public Transform cube;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void PlaceCubes(float cubeRate, Coord[,] coords) {
         for(int x = 0; x < coords.GetLength(0); x++) {
             for(int y = 0; y < coords.GetLength(1); y++) {
