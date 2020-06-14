cbbca1676bb7317fa60d26430184f324045a3cfb
VikingFishBird <oybas@hotmail.com>
2020-06-13 20:54:06 -0700

    CubeyPerlin

--- Insignificance/Assets/Scripts/CubeSpawner.cs
+++ Insignificance/Assets/Scripts/CubeSpawner.cs
@@ -6,7 +6,7 @@ public class CubeSpawner : MonoBehaviour
 {
     public Transform cube;
 
-    public void PlaceCubes(float cubeRate, Coord[,] coords, Transform parent) {
+    public void PlaceCubes(Coord[,] coords, Transform parent, float[,] perlin) {
         string holderName = "Object Holder";
         if (parent.Find(holderName)) {
             DestroyImmediate(transform.Find(holderName).gameObject);
@@ -17,16 +17,26 @@ public class CubeSpawner : MonoBehaviour
 
         for (int x = 0; x < coords.GetLength(0); x++) {
             for(int y = 0; y < coords.GetLength(1); y++) {
-                if(Random.Range(0f, 1.0f) < cubeRate) {
-                    PlaceCubeAtCoord(coords[x, y], mapHolder);
-                }
+                int heightLevel = GetHeightLevelFromPerlin(perlin[x, y]);
+                PlaceCubeAtCoord(coords[x, y], mapHolder, heightLevel);
             }
         }
     }
 
-    public void PlaceCubeAtCoord(Coord coord, Transform parent) {
-        Transform cubey = Instantiate(cube, new Vector3(coord.x, 0.5f, coord.y), Quaternion.Euler(Vector3.right * -90));
+    public void PlaceCubeAtCoord(Coord coord, Transform parent, int heightLevel) {
+        Transform cubey = Instantiate(cube, new Vector3(coord.x, -0.5f + heightLevel, coord.y), Quaternion.Euler(Vector3.right * -90));
         cubey.localScale = new Vector3(50f, 50f, 50f);
         cubey.parent = parent;
+    }
+
+    public int GetHeightLevelFromPerlin(float val) {
+        if(val <= 0.3f)
+            return 0;
+        else if(val <= 0.75f)
+            return 1;
+        else if (val <= 0.9f)
+            return 2;
+        else
+            return 3;
     }
 }
--- Insignificance/Assets/Scripts/MapGenerator.cs
+++ Insignificance/Assets/Scripts/MapGenerator.cs
@@ -11,6 +11,7 @@ public class MapGenerator : MonoBehaviour
     public Coord[,] coordinates;
     [Range(0.0f, 1.0f)]
     public float cubeRate;
+    public float noiseMapScale;
     CubeSpawner cubey;
 
     // Start is called before the first frame update
@@ -38,17 +39,47 @@ public class MapGenerator : MonoBehaviour
 
         for (int x = 0; x < mapSize.x; x++) {
             for(int y = 0; y < mapSize.y; y++) {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
-                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                //Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
+                //Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
 
-                newTile.parent = mapHolder;
+                //newTile.parent = mapHolder;
 
                 coordinates[x,y] = new Coord(-mapSize.x / 2 + 0.5f + x, -mapSize.y / 2 + 0.5f + y);
+                
             }
         }
 
-        cubey.PlaceCubes(cubeRate, coordinates, transform);
+        float[,] noiseMap = GenerateNoiseMap(noiseMapScale);
+        cubey.PlaceCubes(coordinates, transform, noiseMap);
+
     }
 
-    
+    public float[,] GenerateNoiseMap(float scale) {
+        float[,] noiseMap = new float[Mathf.RoundToInt(mapSize.x), Mathf.RoundToInt(mapSize.y)];
+        float offSetX = Random.Range(-100000, 100000);
+        float offSetY = Random.Range(-100000, 100000);
+
+        float halfWidth = mapSize.x / 2f;
+        float halfHeight = mapSize.y / 2f;
+
+        for (int x = 0; x < mapSize.x; x++) {
+            //List<string> perlinNoiseThings = new List<string>();
+
+            for (int y = 0; y < mapSize.y; y++) {
+                float sampleX = (x - halfWidth + offSetX) / scale;
+                float sampleY = (y - halfWidth + offSetY) / scale;
+
+                float perlinValue = Mathf.Clamp(Mathf.PerlinNoise(sampleX, sampleY), 0, 1);
+                
+                noiseMap[x, y] = perlinValue;
+                //perlinNoiseThings.Add(Mathf.PerlinNoise(sampleX, sampleY).ToString());
+            }
+
+            //print(string.Join(" | ", perlinNoiseThings));
+            //perlinNoiseThings = new List<string>();
+        }
+
+        return noiseMap;
+
+    }   
 }
