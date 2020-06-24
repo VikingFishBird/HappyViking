03f2c26dbab61dd0e29e6e8857942c1fce73d2dc
VikingFishBird <oybas@hotmail.com>
2020-06-24 01:02:40 -0700

    dd

--- Insignificance/Assets/Scripts/MountainData.cs
+++ Insignificance/Assets/Scripts/MountainData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MountainData
 {
-    public Mountain[] mountains;
+    public static Mountain[] mountains;
     
     public MountainData() {
         // Set mountains array. Add 1 to length when you add a mountain.
