5fc797613cbc99c9f4cbfac6fc1a53e775a9fe0a
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2020-06-24 00:13:02 -0700

    agoeuybhfrlidvn cm,gzcxhryboulvb
    piujnac rfgvp,mi uhbazxdtghbp;,imuj nhazdghbpijunb

--- Insignificance/Assets/Scripts/MountainData.cs
+++ Insignificance/Assets/Scripts/MountainData.cs
@@ -9,12 +9,46 @@ public class MountainData
     public MountainData() {
         // Set mountains array. Add 1 to length when you add a mountain.
         mountains = new Mountain[1];
-
+        #region Mountain1
         // Add a new Mountain
-        mountains[0] = new Mountain(2, 5, 5); // Height, Length, Width
+        mountains[0] = new Mountain(2, 4, 4); // Height, Length, Width
         BlockData[,,] bA = mountains[0].blockArray;
         // Add a block to the bloackArray[height, length, width]
         bA[0, 0, 0] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[0, 0, 1] = new BlockData(BlockType.Corner, 180); // Type, rotation (degrees)
+        bA[0, 0, 2] = new BlockData(BlockType.Side, 180); // Type, rotation (degrees)
+        bA[0, 0, 3] = new BlockData(BlockType.Corner, 270); // Type, rotation (degrees)
+        bA[0, 1, 0] = new BlockData(BlockType.Corner, 180); // Type, rotation (degrees)
+        bA[0, 1, 1] = new BlockData(BlockType.Wedge, 180); // Type, rotation (degrees)
+        bA[0, 1, 2] = new BlockData(BlockType.Land, 0); // Type, rotation (degrees)
+        bA[0, 1, 3] = new BlockData(BlockType.Side, 270); // Type, rotation (degrees)
+        bA[0, 2, 0] = new BlockData(BlockType.Side, 90); // Type, rotation (degrees)
+        bA[0, 2, 1] = new BlockData(BlockType.Wedge, 0); // Type, rotation (degrees)
+        bA[0, 2, 2] = new BlockData(BlockType.Side, 0); // Type, rotation (degrees)
+        bA[0, 2, 3] = new BlockData(BlockType.Corner, 0); // Type, rotation (degrees)
+        bA[0, 3, 0] = new BlockData(BlockType.Corner, 90); // Type, rotation (degrees)
+        bA[0, 3, 1] = new BlockData(BlockType.Corner, 0); // Type, rotation (degrees)
+        bA[0, 3, 2] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[0, 3, 3] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 0, 0] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 0, 1] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 0, 2] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 0, 3] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 1, 0] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 1, 1] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 1, 2] = new BlockData(BlockType.Peak, 0); // Type, rotation (degrees)
+        bA[1, 1, 3] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 2, 0] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 2, 1] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 2, 2] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 2, 3] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 3, 0] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 3, 1] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 3, 2] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+        bA[1, 3, 3] = new BlockData(BlockType.Air, 0); // Type, rotation (degrees)
+
+        #endregion
+
     }
 }
 
