791e26361f6171db760efbce81028f952fbc90d2
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2020-07-14 22:54:06 -0700

    drci6yf
    itrfyxk

--- Insignificance/Assets/Scripts/Sun.cs
+++ Insignificance/Assets/Scripts/Sun.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Sun : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float duration = 10.0F;
+    public Color color0 = Color.red;
+    public Color color1 = Color.blue;
+    public Light lt;
     void Start()
     {
-        
+        lt = GetComponent<Light>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, 5f * Time.fixedDeltaTime);
-        transform.LookAt(Vector3.zero);
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
     }
 }
