8fbbf41c49ce3b02480c5e0dec646ffbca3d1dbb
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2021-06-14 14:34:30 -0700

    legacy 1
    WEFwefSEF

---
+++ Insignifigance Escape 2 Africa/Assets/Scripts/MusicFade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicFade : MonoBehaviour
+{
+    private static MusicFade instance;
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Legacy Curse of the Black Pearl/Assets/Scripts/menuButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuButtonScript : MonoBehaviour
+{
+    public GameObject menuHud;
+
+    public void activateMenuHud()
+    {
+        if (menuHud.activeSelf)
+        {
+            menuHud.SetActive(false);
+        }
+        else
+            menuHud.SetActive(true);
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            if (menuHud.activeSelf)
+            {
+                menuHud.SetActive(false);
+            }
+    }
+}
