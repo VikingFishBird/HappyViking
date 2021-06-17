f514a351176d603815a64bce40826cff3b9b9fc0
thehappybuilder <66582744+thehappybuilder@users.noreply.github.com>
2021-06-17 16:18:10 -0700

    aerg;oihnbuawergftiugbp;egawrf
    [iubnaergp;iubaerg

---
+++ Legacy Curse of the Black Pearl/Assets/Scripts/OptionsHudCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsHudCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject optionHud;
+
+    void start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && optionHud.activeSelf)
+            if (optionHud.activeSelf)
+            {
+                optionHud.SetActive(false);
+            }
+       
+    }
+}
